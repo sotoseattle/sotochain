849a986a2c294f850beaefd9e23ffc39ca32b905
sotoseattle <sotoseattle@gmail.com>
2021-08-27 12:05:05 -0700

    clean up error mgmt and flow

--- lib/ledger/fetch.ex
+++ lib/ledger/fetch.ex
@@ -1,78 +1,93 @@
 defmodule Ledger.Fetch do
   alias Ledger.Tx
   alias Ledger.TxOut
+  alias Ledger.Fetch
+
+  defstruct valid: nil, tx: nil, error: nil, raw: nil
 
   def fetch(tx_id) do
     tx_id
     |> retrieve_tx_online()
+    |> parse_tx_blurb()
     |> check_unspent_utxo()
-    |> expand_inputs()
+    |> retrieve_prev_outputs()
     |> check_fee()
   end
 
+  @doc "get the hex blur of a transaction from an online source"
   def retrieve_tx_online(tx_hex) do
-    with {:ok, raw} <- outside_query(tx_hex) do
-      raw
-      |> Tx.parse()
-      |> Map.put(:meta, %{status: :ok})
+    case HTTPoison.get("https://mempool.space/api/tx/#{tx_hex}/hex") do
+      {:ok, %{body: raw}} ->
+        %Fetch{valid: true, raw: raw}
+
+      err ->
+        %Fetch{valid: false, error: err}
+    end
+  end
+
+  @doc "parse the transaction into its struct"
+  def parse_tx_blurb(%{valid: false} = fo), do: fo
+
+  def parse_tx_blurb(fo) do
+    with %Tx{} = tx <- Tx.parse(fo.raw) do
+      %{fo | tx: tx, raw: nil}
     else
-      err -> err
+      err -> %{fo | valid: false, error: err}
     end
   end
 
   # I cannot check so I consider everything is ok
-  def check_unspent_utxo(%Tx{meta: %{status: :ok}} = tx), do: tx
-  def check_unspent_utxo(err), do: err
+  def check_unspent_utxo(fo), do: fo
 
-  def expand_inputs(%Tx{meta: %{status: :ok}} = tx) do
+  def retrieve_prev_outputs(%{valid: false} = fo), do: fo
+
+  def retrieve_prev_outputs(fo) do
     updated_inputs =
-      tx
+      fo
+      |> Map.get(:tx)
       |> Map.get(:inputs)
       |> Enum.map(fn txin ->
-        case retrieve_out_online(txin.prev_tx, txin.prev_idx) do
-          %TxOut{} = txout -> %{txin | meta: txout}
-          _ -> %{txin | meta: :error}
-        end
+        %{txin | meta: extract_output(txin.prev_tx, txin.prev_idx)}
       end)
 
-    tx
-    |> Map.put(:inputs, updated_inputs)
+    %{fo | tx: %{fo.tx | inputs: updated_inputs}}
     |> invalidate_if_errors()
   end
 
-  def expand_inputs(err), do: err
-
-  defp invalidate_if_errors(tx) do
-    if Enum.any?(tx.inputs, fn i -> i.meta == :error end) do
-      %{tx | meta: %{status: :error}}
-    else
-      tx
-    end
-  end
-
-  def retrieve_out_online(hex, idx) do
+  def extract_output(hex, idx) do
     with hex <- Util.hex_lit_2_big(hex),
-         %Tx{} = tx <- retrieve_tx_online(hex) do
-      tx
+         %{valid: true} = fo <- retrieve_tx_online(hex),
+         %{valid: true} = fo <- parse_tx_blurb(fo) do
+      fo
+      |> Map.get(:tx)
       |> Map.get(:outputs)
       |> Enum.at(idx)
     else
-      err -> err
+      bad_fo -> bad_fo.error
     end
   end
 
-  def check_fee(%Tx{meta: %{status: :ok}} = tx) do
+  defp invalidate_if_errors(fo) do
+    if Enum.all?(fo.tx.inputs, fn i -> is_struct(i.meta, TxOut) end) do
+      fo
+    else
+      %{fo | valid: false, error: "previous tx outputs unavailable"}
+    end
+  end
+
+  def check_fee(%{valid: false} = fo), do: fo
+
+  def check_fee(fo) do
+    tx = fo.tx
     sat_in = tx.inputs |> Enum.map(fn i -> i.meta.amount end) |> Enum.sum()
     sat_ot = tx.outputs |> Enum.map(fn o -> o.amount end) |> Enum.sum()
     fee = sat_in - sat_ot
 
     case fee >= 0 do
-      true -> %{tx | meta: %{fee: fee}}
-      _ -> %{tx | meta: %{status: :error, fee: fee}}
+      true -> %{fo | tx: %{fo.tx | meta: %{fee: fee}}}
+      _ -> %{fo | valid: false, error: "insuficient fee: #{fee} sat"}
     end
   end
-
-  def check_fee(err), do: err
 
   def outside_query(tx) do
     case HTTPoison.get("https://mempool.space/api/tx/#{tx}/hex") do
